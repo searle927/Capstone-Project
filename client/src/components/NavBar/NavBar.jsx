39cd81efcb0c8df3e90b6bcb05158cc537e9cd81
searle927 <searle927@users.noreply.github.com>
2020-08-11 11:21:16 -0400

    added a navbar - not much styling done yet

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div className="Nav">
+          <h2>R+S Wedding</h2>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/details">Details</Link>
+              </li>
+              <li>
+                <Link to="/destination">Destination</Link>
+              </li>
+              <li>
+                <Link to="/advice">Advice</Link>
+              </li>
+              <li>
+                <Link to="/registry">Registry</Link>
+              </li>
+              <li>
+                <Link to="/RSVP">RSVP</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+    );
+  }
+  
+export default NavBar
