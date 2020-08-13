53a9f0765d8f68b4b656f8956081bfe3aee4ba75
searle927 <searle927@users.noreply.github.com>
2020-08-13 19:24:48 -0400

    fixed icons in registry

--- client/src/components/Registry/Registry.jsx
+++ client/src/components/Registry/Registry.jsx
@@ -4,27 +4,32 @@ import './Registry.css'
 function Registry() {
     return (
         <div className="registry">
-            <h4>We thank you for any gifts you decide to give us. Here is a list of places we enjoy...</h4>
-            <h2>
+            <h4 className="tygifts">We thank you for any gifts you decide to give us. Here is a list of places we enjoy...</h4>
+                <div className="registrylist">
             <a href="amazon.com">
-                    <img src="https://i.imgur.com/r55RZgx.jpg" alt="amazon"></img>
-                </a>
+                    <img src="https://i.imgur.com/XPsQ2AV.png" 
+                    alt="amazon" className="amazon"></img>
+                </a><br />
                 <a href="potterybarn.com">
-                    <img src="https://i.imgur.com/2aG6ypS.jpg" alt="potterybarn"></img>
-                </a>
-                <a href="williamsonoma.com">
-                    <img src="https://i.imgur.com/rGXmJRc.jpg" alt="williamsonoma"></img>
-                </a>
+                    <img src="https://i.imgur.com/e1jxIus.png" alt="potterybarn" className="potterybarn"></img>
+                </a><br />
+                <a href="williamssonoma.com">
+                    <img src="https://i.imgur.com/P4Sbktc.png" 
+                    alt="williamssonoma" className="williamssonoma"></img>
+                </a><br />
                 <a href="crateandbarrel.com">
-                    <img src="https://i.imgur.com/svkmx85.jpg" alt="crateandbarrel"></img>
-                </a>
+                    <img src="https://i.imgur.com/hqgflRC.png" 
+                    alt="crateandbarrel" className="crateandbarrel"></img>
+                </a><br />
                 <a href="target.com">
-                    <img src="https://i.imgur.com/v9mhr4X.png" alt="target"></img>
+                    <img src="https://i.imgur.com/5zm0swf.png" 
+                    alt="target" className="target"></img>
+                </a><br />
+                <a href="nordstrom.com">
+                    <img src="https://i.imgur.com/KxePbVc.png" 
+                    alt="nordstrom" className="nordstrom"></img>
                 </a>
-                <a href="bloomingdales.com">
-                    <img src="https://i.imgur.com/HpvdJqa.jpg" alt="bloomingdales"></img>
-                </a>
-            </h2>
+                </div>
         </div>
     )
 }
