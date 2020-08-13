6f346538e2a785cf8e1f1908bb7db7307db1eb18
searle927 <searle927@users.noreply.github.com>
2020-08-12 23:58:47 -0400

    added text for details page

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Details.css'
 
 function Details() {
     return (
-        <div>
-            <h1>Details Page</h1>
+        <div className="wcontainer">
+            <h1 className="ws">Who</h1>
+            <h3 className="description">Our dearest friends & family from near and far</h3>
+            <h1 className="ws">Where</h1>
+            <h3 className="description">The Royalton</h3>
+            <h1 className="ws">When</h1>
+            <h3 className="description">October 23rd, 2021</h3>
         </div>
     )
 }
