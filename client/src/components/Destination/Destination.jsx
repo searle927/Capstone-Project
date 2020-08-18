9b331bd6d9d1fe10abb75735bc4266f9546aaa97
searle927 <searle927@users.noreply.github.com>
2020-08-17 23:37:28 -0400

    finished mobile view

--- client/src/components/Destination/Destination.jsx
+++ client/src/components/Destination/Destination.jsx
@@ -8,7 +8,7 @@ function Destination() {
         <div className='destinationcontainer'>
             <h1 className="destination">The Destination</h1>
             <h2 className='destinationloc'>The Royalton, Atigua and Barbuda</h2>
-            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem et eveniet a, odit labore molestias dolore itaque facilis illo placeat repudiandae asperiores ratione doloribus aliquam harum aliquid necessitatibus enim fuga.</h3>
+            <h3 className="destinationmessage">As you all know very well, Ryan and I love to travel. So we thought it would be very fitting that our wedding be in one of the most beautiful places we traveled to. We suggest getting here a couple days early because we'd love to hang out and spend time with everyone before the wedding day. Please <a className="link" href="https://www.royaltonresorts.com/royalton-antigua/accommodations/chairman-overwater-bungalows">click here</a> to book your hotels if you'll be staying at the Royalton as well. <br /><br /> We are super excited to see everyone at the wedding!</h3>
         </div>
         </>
     )
